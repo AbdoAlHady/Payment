fe9934f91d2e06da73431f1e1edc4bc2a60a0f30
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2024-08-11 23:16:50 +0300

    - make image expanded in free space

--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -25,27 +25,25 @@ class MyCartView extends StatelessWidget {
           onPressed: () {},
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const SizedBox(height: 25),
-              Image.asset('assets/images/basket_image.png'),
-              const SizedBox(height: 25),
-              const OrderInfoItem(title: 'Order Subtotal', price: 42.97),
-              const SizedBox(height: 3),
-              const OrderInfoItem(title: 'Discount', price: 0),
-              const SizedBox(height: 3),
-              const OrderInfoItem(title: 'Shipping', price: 8),
-              const SizedBox(height: 10),
-              const Divider(thickness: 2.0, height: 24),
-              const TotalPrice(title: 'Total', value: 50.97),
-              const SizedBox(height: 16),
-              CustomButton(onPressed: () {}),
-              const SizedBox(height: 12),
-            ],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+            Expanded(child: Image.asset('assets/images/basket_image.png')),
+            const SizedBox(height: 25),
+            const OrderInfoItem(title: 'Order Subtotal', price: 42.97),
+            const SizedBox(height: 3),
+            const OrderInfoItem(title: 'Discount', price: 0),
+            const SizedBox(height: 3),
+            const OrderInfoItem(title: 'Shipping', price: 8),
+            const SizedBox(height: 10),
+            const Divider(thickness: 2.0, height: 24),
+            const TotalPrice(title: 'Total', value: 50.97),
+            const SizedBox(height: 16),
+            CustomButton(onPressed: () {}),
+            const SizedBox(height: 12),
+          ],
         ),
       ),
     );
