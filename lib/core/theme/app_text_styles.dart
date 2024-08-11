f90963e9430e510565904285d9608ee021b70aa2
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2024-08-11 18:24:50 +0300

    - added some text styles - added appBar for myCart view

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles {
+  static const TextStyle font18BlackW400 = TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+    height: 0,
+  );
+  static const TextStyle font24BlackW600 = TextStyle(
+    fontSize: 24,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle font20BlackW400 = TextStyle(
+    fontSize: 24,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle font25BlackW500 = TextStyle(
+    fontSize: 25,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+}
--- lib/features/checkout/presentation/views/my_cart_view.dart
+++ lib/features/checkout/presentation/views/my_cart_view.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/theme/app_text_styles.dart';
+
 class MyCartView extends StatelessWidget {
   const MyCartView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My Cart',
+          style: AppTextStyles.font25BlackW500,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
+          onPressed: () {},
+        ),
+      ),
+    );
   }
 }
