905418062ace8320ceb0ccd326f8b167ebb40bab
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2024-08-13 03:04:18 +0300

    - added credit card ui

--- lib/features/checkout/presentation/widgets/payment_details_view_body.dart
+++ lib/features/checkout/presentation/widgets/payment_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:payment_app/features/checkout/presentation/widgets/payment_methods_list_view.dart';
 
 class PaymentDetailsViewBody extends StatelessWidget {
@@ -9,6 +10,36 @@ class PaymentDetailsViewBody extends StatelessWidget {
     return const Column(
       children: [
         PaymentMethodsListView(),
+        CustomCreditCart(),
+      ],
+    );
+  }
+}
+
+class CustomCreditCart extends StatefulWidget {
+  const CustomCreditCart({super.key});
+
+  @override
+  State<CustomCreditCart> createState() => _CustomCreditCartState();
+}
+
+class _CustomCreditCartState extends State<CustomCreditCart> {
+  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
+
+  bool showBackView = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          onCreditCardWidgetChange: (CreditCardBrand creditCardBrand) {},
+        ),
       ],
     );
   }
